0ac81d18f80cb034ab1eb972cadd5efb1a4bdd0f
Tedros <tedtfu@gmail.com>
2025-10-29 16:23:33 -0400

    + Home page component,  + tailwind css , vite config

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Home from './components/Home'
+
+function App() {
+  return (
+    <div>
+      {/* Test Tailwind */}
+      <div className="bg-blue-500 text-white p-4 text-center">
+        ✅ Tailwind is working!
+      </div>
+      
+  
+      <Home />
+    </div>
+  )
+}
+
+export default App
